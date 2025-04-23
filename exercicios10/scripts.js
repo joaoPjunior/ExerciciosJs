7f5433e46253a9043006797ac989be14a8616868
joaoPjunior <joaoomenordacasa@gmail.com>
2025-04-23 12:23:10 -0300

    exerciciohj10

--- exercicios10/scripts.js
+++ exercicios10/scripts.js
@@ -1,29 +1,19 @@
 let calculadora = {
-    somar: function(a,b){
+    somar: function(a , b){
         return a + b;
     },
-    subtrair: function(a,b){
+    subtrair: function(a , b){
         return a - b;
     },
-    multiplicar: function(a,b){
-        return a * b;
-    },
-    dividir: function(a,b){
+    dividir: function(a , b){
         return a / b;
+    },
+    multiplicar: function(a , b){
+        return a * b;
     }
-}
+};
 
-
-console.log(calculadora.somar(2,4))
-console.log(calculadora.subtrair(2,4))
-console.log(calculadora.multiplicar(2,4))
-console.log(calculadora.dividir(2,4))
-
-
-const arr = []
-
-for( let i = 0; i < 5; i++){
-    arr.push(i + 5 );
-}
-
-console.log(arr);
+console.log(calculadora.somar(2,4));
+console.log(calculadora.dividir(10,15));
+console.log(calculadora.subtrair(20,10));
+console.log(calculadora.multiplicar(4,4));
